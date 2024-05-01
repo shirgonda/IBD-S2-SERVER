ed3830c8fd86aacb4e3573996f09562fa8669104
Lirikran <liri.kranzler@gmail.com>
2024-05-01 23:52:24 +0300

    last update

--- finalproj/finalproj/obj/Debug/net6.0/finalproj.AssemblyInfo.cs
+++ finalproj/finalproj/obj/Debug/net6.0/finalproj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("finalproj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb38d7c87ad27f5c19e08fcbe68fc92061ea845")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21c00a5055e0e188a64d6ab4f69200dc054d3228")]
 [assembly: System.Reflection.AssemblyProductAttribute("finalproj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("finalproj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

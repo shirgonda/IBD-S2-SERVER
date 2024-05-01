3cb38d7c87ad27f5c19e08fcbe68fc92061ea845
Lirikran <liri.kranzler@gmail.com>
2024-05-01 23:01:12 +0300

    התראות

---
+++ finalproj/finalproj/BL/Alert.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using finalproj.BL;
+
+namespace finalproj.BL
+{
+    public class Alert
+    {
+        private int alertId;
+        private int eventId;
+        private DateTime alertTime;
+        private string aname;
+        private string arepit;
+
+        public static List<Alert> Alerts = new List<Alert>();
+
+        public Alert(int eventId, DateTime alertTime, string aname, string arepit)
+        {
+            EventId = eventId;
+            AlertTime = alertTime;
+            Aname = aname;
+            Arepit = arepit;
+        }
+
+        public Alert() { }
+
+        public int AlertId { get => alertId; set => alertId = value; }
+        public int EventId { get => eventId; set => eventId = value; }
+        public DateTime AlertTime { get => alertTime; set => alertTime = value; }
+        public string Aname { get => aname; set => aname = value; }
+        public string Arepit { get => arepit; set => arepit = value; }
+
+        // Methods for CRUD operations can be added similarly as in the User class
+    }
+}
---
+++ finalproj/finalproj/BL/CalendarEvent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using finalproj.BL;
+
+namespace finalproj.BL
+{
+    public class CalendarEvent
+    {
+        private int eventId;
+        private int userId;
+        private DateTime startTime; 
+        private DateTime endTime;   
+        private string name;
+        private string location;
+        private string repit;
+        private int day;
+        private int month;
+        private int year;
+
+        public static List<CalendarEvent> CalendarEvents = new List<CalendarEvent>();
+
+        public CalendarEvent(int userId, DateTime startTime, DateTime endTime, string name, string location, string repit, int day, int month, int year)
+        {
+            UserId = userId;
+            StartTime = startTime;
+            EndTime = endTime;
+            Name = name;
+            Location = location;
+            Repit = repit;
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        public CalendarEvent() { }
+
+        public int EventId { get => eventId; set => eventId = value; }
+        public int UserId { get => userId; set => userId = value; }
+        public DateTime StartTime { get => startTime; set => startTime = value; }
+        public DateTime EndTime { get => endTime; set => endTime = value; }
+        public string Name { get => name; set => name = value; }
+        public string Location { get => location; set => location = value; }
+        public string Repit { get => repit; set => repit = value; }
+        public int Day { get => day; set => day = value; }
+        public int Month { get => month; set => month = value; }
+        public int Year { get => year; set => year = value; }
+
+    }
+}
